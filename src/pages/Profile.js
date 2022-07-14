193f00b38567e47b5e5626bc434a348d1658fe4b
Otávio Azevedo <otavioad1997@gmail.com>
2022-07-14 20:33:58 -0300

    Requisito 4

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
-export class Profile extends Component {
+class Profile extends Component {
   render() {
     return (
       <div>
