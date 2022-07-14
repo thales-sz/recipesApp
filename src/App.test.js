acaf5394f79b6d9534e76cb815721d7d06f4147c
Otávio Azevedo <otavioad1997@gmail.com>
2022-07-14 20:19:54 -0300

    Requisito 4

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import { renderWithRouterAndStore } from './testConfig'
 
-test('Farewell, front-end', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('1 - Página de Login', () => {
+  test('Espera os inputs de email e senha e o botão com o texto \'Enter\'', () => {
+    renderWithRouterAndStore(<App />);
+
+    const email = screen.getByTestId('email-input');
+    const password = screen.getByTestId('email-input');
+    const btnEnter = screen.getByText(/Enter/i);
+
+
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(btnEnter).toBeInTheDocument();
+  });
+  
+  test('Verifica se o botão com o data-testid="login-submit-btn" se encontra desabilitado inicialmente e após inserir um email e senha nos formatos válidos ele habilita', () => {
+    const { store } = renderWithRouterAndStore(<App />);
+    const btnLogin = screen.getByTestId('login-submit-btn');
+    expect(btnLogin).toBeDisabled();
+
+    const email = screen.getByTestId('email-input');
+    const password = screen.getByTestId('email-input');
+
+    userEvent.type(email, 'teste@tes');
+    userEvent.type(password, 'teste');
+    expect(btnLogin).toBeDisabled();
+
+    userEvent.type(email, 'teste@teste.com');
+    userEvent.type(password, 'teste1');
+    expect(btnLogin).toBeDisabled();
+
+    userEvent.type(email, 'teste@testecom');
+    userEvent.type(password, 'teste12');
+    expect(btnLogin).toBeDisabled();
+
+    userEvent.type(email, 'teste@teste.com');
+    userEvent.type(password, 'teste123');
+    expect(store.getState().disabled).toBeFalsy();
+  })
+})
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,28 +3,52 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Login extends React.Component {
+  state={
+    email: '',
+    password: '',
+    disabled: true,
+  }
+
+  onButtonChange = () => {
+    const { password, email } = this.state;
+    const NUM = 6;
+    if (password.length > NUM && email.match(/\S+@\S+\.\S+/)) {
+      return this.setState({ disabled: false });
+    } this.setState({ disabled: true });
+  };
+
+  onInputChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value }, this.onButtonChange);
+  }
+
   render() {
+    const { email, password, disabled } = this.state;
     return (
       <form>
-        <label htmlFor="email-input">
+        <label htmlFor="email">
           Email:
           <input
             type="email"
-            id="email-input"
+            id="email"
+            value={ email }
             data-testid="email-input"
+            onChange={ this.onInputChange }
           />
         </label>
-        <label htmlFor="password-input">
+        <label htmlFor="password">
           Senha:
           <input
             type="password"
-            id="password-input"
+            id="password"
+            value={ password }
             data-testid="password-input"
+            onChange={ this.onInputChange }
           />
         </label>
         <button
           type="button"
           data-testid="login-submit-btn"
+          disabled={ disabled }
         >
           Enter
         </button>
