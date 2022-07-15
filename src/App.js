b82f38a37c7719d959235229946656458e6a0a8a
Thales Chagas <thales.souz@outlook.com>
2022-07-15 17:48:19 -0300

    Corrigindo testes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
-import MainPage from './pages/Recipes';
+import Recipes from './pages/Recipes';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/foods" component={ MainPage } />
-      <Route path="/drinks" component={ MainPage } />
+      <Route path="/foods" component={ Recipes } />
+      <Route path="/drinks" component={ Recipes } />
       <Route path="/profile" component={ Profile } />
     </Switch>
   );
