fab3d665bc627cf756a19e4556ae1abb4860a726
Thales Chagas <thales.souz@outlook.com>
2022-07-18 21:32:59 -0300

    Requisito 17 e 14

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import profileIcon from '../images/profileIcon.svg';
-import searchIcon from '../images/searchIcon.svg';
-import SearchBar from './SearchBar';
+import profileIcon from '../../images/profileIcon.svg';
+import searchIcon from '../../images/searchIcon.svg';
+import SearchBar from '../SearchBar/SearchBar';
 import './Header.css';
 
 function Header({ title, foodOrDrink }) {
