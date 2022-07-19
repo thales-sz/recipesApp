4ac8bf102b9bcb557d007fc7574b3ef3e0b410a9
Otávio Azevedo <otavioad1997@gmail.com>
2022-07-18 22:15:31 -0300

    Requisito 20

--- src/components/Cards/DrinksCards.js
+++ src/components/Cards/DrinksCards.js
@@ -6,11 +6,15 @@ import { addRecipeDrinks } from '../../redux/actions';
 function DrinksCards() {
   const dispatch = useDispatch();
   const [arrayDrinks, setArrayDrinks] = useState();
+  const [arrayCategoryDrinks, setCategoryCategoryDrinks] = useState();
+
   useEffect(() => {
     const getDrinks = async () => {
       const drinks = await getAPI('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const categorysDrinks = await getAPI('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
       dispatch(addRecipeDrinks(drinks));
       setArrayDrinks(drinks.drinks);
+      setCategoryCategoryDrinks(categorysDrinks.drinks);
       return drinks;
     };
     return getDrinks();
@@ -20,12 +24,27 @@ function DrinksCards() {
   const imageCard = (food) => `${food.strDrinkThumb}`;
   const dataTestName = (index) => `${index}-card-name`;
   const dataTestImg = (index) => `${index}-card-img`;
+  const dataTestCat = (cat) => `${cat}-category-filter`;
 
   const TWELVE = 12;
   const twelveDrinks = arrayDrinks?.slice(0, TWELVE);
+  const FIVE = 5;
+  const categoryFiveDrinks = arrayCategoryDrinks?.slice(0, FIVE);
 
   return (
     <div>
+      <div>
+        <button type="button" data-testid="all-category">All</button>
+        {categoryFiveDrinks?.map((cat, index) => (
+          <button
+            type="button"
+            key={ index }
+            data-testid={ dataTestCat(cat.strCategory) }
+          >
+            {cat.strCategory}
+          </button>
+        ))}
+      </div>
       { twelveDrinks?.map((drink, index) => (
         <section
           key={ drink.idDrink }
--- src/components/Cards/FoodCards.js
+++ src/components/Cards/FoodCards.js
@@ -6,10 +6,13 @@ import { addRecipeFoods } from '../../redux/actions';
 function FoodCards() {
   const dispatch = useDispatch();
   const [arrayFoods, setArrayFoods] = useState();
+  const [arrayCategoryFoods, setCategoryCategoryFoods] = useState();
   useEffect(() => {
     const getFoods = async () => {
       const foods = await getAPI('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const categorys = await getAPI('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
       dispatch(addRecipeFoods(foods));
+      setCategoryCategoryFoods(categorys.meals);
       setArrayFoods(foods.meals);
       return foods;
     };
@@ -20,12 +23,27 @@ function FoodCards() {
   const imageCard = (food) => `${food.strMealThumb}`;
   const dataTestName = (index) => `${index}-card-name`;
   const dataTestImg = (index) => `${index}-card-img`;
+  const dataTestCat = (cat) => `${cat}-category-filter`;
 
   const TWELVE = 12;
+  const FIVE = 5;
   const twelve = arrayFoods?.slice(0, TWELVE);
+  const categoryFive = arrayCategoryFoods?.slice(0, FIVE);
 
   return (
     <div>
+      <div>
+        <button type="button" data-testid="all-category">All</button>
+        {categoryFive?.map((cat, index) => (
+          <button
+            type="button"
+            key={ index }
+            data-testid={ dataTestCat(cat.strCategory) }
+          >
+            {cat.strCategory}
+          </button>
+        ))}
+      </div>
       { twelve?.map((food, index) => (
         <section
           key={ food.idMeal }
