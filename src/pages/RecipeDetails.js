6c4d5db67cca2de4108bb96721652dd68daef2ee
Otávio Azevedo <otavioad1997@gmail.com>
2022-07-19 18:23:16 -0300

    Requisito 24

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,12 +1,27 @@
 import PropTypes from 'prop-types';
-import React/* , { useEffect }  */from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { getAPI } from '../helpers';
 
 function RecipeDetails({ match: { params: { id }, path } }) {
   console.log(path);
   console.log(id);
+
+  useEffect(() => {
+    const getRecipeDetail = async (endpoint) => {
+      const recipe = await getAPI(`${endpoint}${id}`);
+      console.log(recipe);
+    };
+    if (path === '/drinks/:id') {
+      return getRecipeDetail('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=');
+    }
+    getRecipeDetail('https://www.themealdb.com/api/json/v1/1/lookup.php?i=');
+  }, [id, path]);
+
   return (
-    <div>RecipeDetails</div>
+    <article>
+      <h2>RecipeDetails</h2>
+    </article>
   );
 }
 
